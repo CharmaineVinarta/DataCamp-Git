6266753f5e90afdfba097f20c54ed1a4a8701c49
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-13 00:06:10 -0700

    Create Intermediate_Customize Plot.py

---
+++ Intermediate_Customize Plot.py
@@ -0,0 +1,53 @@
+# LABELS
+
+# GDP per capita on the x-axis (logarithmic scale), life expectancy on the y-axis. The code for this plot is available in the script.
+
+# Import the course packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import the two datasets
+gapminder = pd.read_csv("datasets/gapminder.csv")
+brics = pd.read_csv("datasets/brics.csv")
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+
+# Ticks Function 
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val,tick_lab)
+
+# After customizing, display the plot
+plt.show()
