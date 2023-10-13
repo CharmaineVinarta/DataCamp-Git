cc745aff64e1b4f3d7a9b1b2be13ac827ed069a2
Charmaine Vinarta <120625052+CharmaineVinarta@users.noreply.github.com>
2023-10-13 14:44:00 -0700

    Create Panda Dictionary DataFrame.py

---
+++ Panda Dictionary DataFrame.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# fix row labels (i.e. the labels for the different observations) were automatically set to integers from 0 up to 6?
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Print cars again
+print(cars)
+
